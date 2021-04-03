a749caadb6f922ec535ed570dc8ac6a691850f7d
Justplayingwithx <75589084+Justplayingwithx@users.noreply.github.com>
2021-04-03 14:46:31 -0400

    add the thing andddddddddd commit!

--- sketch.js
+++ sketch.js
@@ -3,6 +3,17 @@
 
 function setup() {
   createCanvas(500,500);
+  
+  signUpFieldUsername = createInput("username")
+signUpFieldUsername.position(140,350)
+
+
+signUpFieldPassword = createInput("password",'password')
+signUpFieldPassword.position(140,400)
+
+signUpButton = createButton("Sign up")
+signUpButton.position(190,450)
+
 
 
   
